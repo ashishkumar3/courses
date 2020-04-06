09a813c4e20de468e6d64832fb43c6b72da80bce
ashishkumar3 <ashishk4371@gmail.com>
2020-04-06 13:09:09 +0530

    user login :key:

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import * as Joi from '@hapi/joi';
+import { Router } from '@angular/router';
+
+const LOGIN_URL = 'http://localhost:3000/auth/login';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  logingIn: boolean = false;
+  errorMessage: string = null;
+
+  user: { email: string; password: string; } = {
+    email: null,
+    password: null
+  };
+
+  schema: Joi.Schema = Joi.object({
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+    password: Joi.string()
+      .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+    confirmPassword: Joi.ref('password')
+  });
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.login();
+  }
+
+  isUserValid() {
+    this.errorMessage = null;
+    const result = this.schema.validate(this.user);
+
+    if (!result.error) {
+      return true;
+    } else if (result.error.details) {
+      this.errorMessage = result.error.details[0].message;
+    }
+  }
+
+  login() {
+    console.log(this.user.email, this.user.password);
+    if (this.isUserValid()) {
+      this.logingIn = true;
+      fetch(LOGIN_URL, {
+        method: 'POST',
+        body: JSON.stringify(this.user),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(res => {
+        this.logingIn = false;
+        if (res.ok) return res.json();
+        return res.json().then(err => {
+          throw new Error(err.message);
+        });
+      }).then(user => {
+        console.log(user);
+        this.router.navigate(['home']);
+      }).catch(err => {
+        this.errorMessage = err.message;
+      });
+    }
   }
 
 }
