248912222c22e8d6383c697823ef30d104cfab69
ashishkumar3 <ashishk4371@gmail.com>
2020-04-07 02:44:30 +0530

    verify token, logout

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,11 +8,32 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
 
+  API_URL: string = 'http://localhost:3000';
+  user;
+
   constructor(private router: Router) {
     // console.log(this.router.url);
   }
 
   ngOnInit(): void {
+    fetch(this.API_URL, {
+      method: 'GET',
+      headers: {
+        authorization: `Bearer ${localStorage.token}`
+      }
+    }).then(res => res.json()).then(result => {
+      console.log("RESULT", result);
+      if (result.user) {
+        this.user = result.user;
+      } else {
+        this.logout();
+      }
+    }).catch(err => console.log(err));
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
   }
 
 }
